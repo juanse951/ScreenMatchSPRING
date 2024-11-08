78f4aaa23725f70175116c97a08927ba46168fc3
juanse951 <juanse951@gmail.com>
2024-11-08 09:03:57 -0500

    creando screnMatch con spring, se hizo el metodo para consumir la api

---
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,21 @@
+package com.aluracursos.screenmatch;
+
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=ac03da8d");
+		System.out.println(json);
+	}
+}
