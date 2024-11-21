f2e704fe1d8e75f0be8bb192c0d77b250d9c5d80
juanse951 <juanse951@gmail.com>
2024-11-21 09:13:26 -0500

    04 Buscando top 5 series

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -37,6 +37,7 @@ public class Principal {
                     2 - Buscar episodios
                     3 - Mostrar series buscadas
                     4 - Buscar series por titulo
+                    5 - Top 5 mejores series
                                   
                     0 - Salir
                     """;
@@ -56,6 +57,9 @@ public class Principal {
                     break;
                 case 4:
                     buscarSeriesPorTitulo();
+                    break;
+                case 5:
+                    buscarTop5Series();
                     break;
                 case 0:
                     System.out.println("Cerrando la aplicación...");
@@ -138,4 +142,10 @@ public class Principal {
         }
     }
 
+    private void buscarTop5Series(){
+        List<Serie> topSeries = repositorio.findTop5ByOrderByEvaluacionDesc();
+        topSeries.forEach(s ->
+                System.out.println("Serie: " + s.getTitulo() + " Evaluación: " + s.getEvaluacion()));
+
+    }
 }
--- src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -3,8 +3,11 @@ package com.aluracursos.screenmatch.repository;
 import com.aluracursos.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository<Serie, Long> {
    Optional<Serie> findByTituloContainsIgnoreCase(String nombreSerie);
+
+   List<Serie> findTop5ByOrderByEvaluacionDesc();
 }
